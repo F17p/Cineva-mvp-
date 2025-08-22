ed1b5ac10c6524c984d4b007e2d5837348707fbf
F17p <felicianondala95@gmail.com>
2025-08-22 21:56:06 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px", fontFamily: "Arial" }}>
+      <h1>🎬 Bem-vindo ao Cineva</h1>
+      <p>A sua nova plataforma de streaming.</p>
+    </div>
+  );
+}
