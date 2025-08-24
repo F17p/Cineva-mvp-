df9929005b61364e4ad6011801bb8485c595bc67
F17p <felicianondala95@gmail.com>
2025-08-24 15:36:50 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout'
+import Header from '../components/Header'
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
+      <Header />
       <Component {...pageProps} />
-    </Layout>
+    </>
   )
 }
-
-export default MyApp
