cc4cfab98a37b555c8db871f742db237699067dd
F17p <felicianondala95@gmail.com>
2025-08-25 06:05:15 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,3 @@
-import '../styles/globals.css'
-import Header from '../components/Header'
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
-  )
-}
-
 import '@/styles/globals.css'
 import Footer from '../components/Footer'
 
