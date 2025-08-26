2a87cccb6ca7c32d58345dbbb9fa11d97f93df08
F17p <felicianondala95@gmail.com>
2025-08-26 22:52:58 +0100

    Update [id].js

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,30 +1,35 @@
-import { useRouter } from 'next/router'
-import movies from '../../data/movies.json'
+// pages/movies/[id].js
+import { useRouter } from "next/router";
+import movies from "../../data/movies";
 
-export default function MoviePage() {
-  const router = useRouter()
-  const { id } = router.query
+export default function MovieDetail() {
+  const router = useRouter();
+  const { id } = router.query;
 
-  const movie = movies.find(m => m.id.toString() === id)
+  const movie = movies.find((m) => m.id.toString() === id);
 
-  if (!movie) {
-    return <div className="p-8">❌ Filme não encontrado...</div>
-  }
+  if (!movie) return <h1>Filme não encontrado</h1>;
 
   return (
-    <div className="p-8">
+    <main className="p-8">
       <h1 className="text-3xl font-bold mb-4">{movie.title}</h1>
-      <div className="aspect-video mb-4">
-        <iframe
-          className="w-full h-full rounded-xl shadow-lg"
-          src={movie.videoUrl}
-          title={movie.title}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <p className="text-gray-700">{movie.description}</p>
-    </div>
-  )
+      <p className="mb-4">Categoria: {movie.category}</p>
+      
+      <img
+        src={movie.image}
+        alt={movie.title}
+        className="rounded-lg mb-6 w-64"
+      />
+
+      <iframe
+        width="100%"
+        height="400"
+        src={movie.trailer}
+        title={movie.title}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </main>
+  );
 }
